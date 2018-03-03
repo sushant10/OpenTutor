6b755ed018636867af3aee3d1b891a2072dbbcc6
sushant10 <sushdude34@gmail.com>
2018-03-03 15:30:34 -0500

    Button and FindTutor page added

---
+++ app/src/main/java/com/ha/sushantrao/opentutor/FindtutorActivity.java
@@ -0,0 +1,13 @@
+package com.ha.sushantrao.opentutor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindtutorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_findtutor);
+    }
+}
--- app/src/main/java/com/ha/sushantrao/opentutor/MainActivity.java
+++ app/src/main/java/com/ha/sushantrao/opentutor/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ha.sushantrao.opentutor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import info.hoang8f.widget.FButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FButton fTutor= (FButton) findViewById(R.id.findtutor);
+        fTutor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, FindtutorActivity.class));
+            }
+        });
     }
+
 }
