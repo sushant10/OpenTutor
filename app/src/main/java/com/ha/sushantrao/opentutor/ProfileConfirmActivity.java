115820a5cd295ad770a62f7ea541b897f9e05c05
sushant10 <sushdude34@gmail.com>
2018-03-03 22:27:09 -0500

    Update confirm page

--- app/src/main/java/com/ha/sushantrao/opentutor/ProfileConfirmActivity.java
+++ app/src/main/java/com/ha/sushantrao/opentutor/ProfileConfirmActivity.java
@@ -3,9 +3,20 @@ package com.ha.sushantrao.opentutor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ProfileConfirmActivity extends AppCompatActivity {
+
+    TextView description;
+    TextView name;
+    TextView textrating;
+    CircleImageView profiledp;
+    ImageView ratingImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +27,26 @@ public class ProfileConfirmActivity extends AppCompatActivity {
         //back button
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        
-        TextView a = (TextView) findViewById(R.id.test);
-        a.setText(extras.getString("name"));
+
+        String major= extras.getString("major");
+        String namepr= extras.getString("name");
+        String rating= extras.getString("rating");
+        int dp= extras.getInt("profile image");
+
+        description = (TextView) findViewById(R.id.textDescription);
+        name = (TextView) findViewById(R.id.textName);
+        textrating= (TextView) findViewById(R.id.textRating);
+        profiledp = (CircleImageView) findViewById(R.id.imageDp);
+        ratingImage = (ImageView) findViewById(R.id.imageRating);
+
+        name.setText(namepr);
+        description.setText("I am a student at UMass Amherst who has a passion for tutoring in "+major);
+        profiledp.setImageResource(dp);
+        ratingImage.setImageResource(R.drawable.rating);
+        textrating.setText(rating);
+
+       /* TextView a = (TextView) findViewById(R.id.test);
+        a.setText(extras.getString("name"));*/
     }
 
     //on back button clicked
