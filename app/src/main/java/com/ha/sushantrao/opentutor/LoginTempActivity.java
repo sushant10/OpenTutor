b413d3ab74574cc48398e2d971c14d892d3c4d67
sushant10 <sushdude34@gmail.com>
2018-03-04 11:37:28 -0500

    Login temp

--- app/src/main/java/com/ha/sushantrao/opentutor/LoginTempActivity.java
+++ app/src/main/java/com/ha/sushantrao/opentutor/LoginTempActivity.java
@@ -3,6 +3,7 @@ package com.ha.sushantrao.opentutor;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -89,7 +90,8 @@ public class LoginTempActivity extends AppCompatActivity implements LoaderCallba
                 attemptLogin();
             }
         });
-        
+
+
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
@@ -333,7 +335,7 @@ public class LoginTempActivity extends AppCompatActivity implements LoaderCallba
             showProgress(false);
 
             if (success) {
-                finish();
+                startActivity(new Intent(LoginTempActivity.this, MainActivity.class));;
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
