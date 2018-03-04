087c2d1936c03c721deb9cb79ccc866dd2641392
sushant10 <sushdude34@gmail.com>
2018-03-04 12:58:17 -0500

    Update // before presentation

---
+++ app/src/main/java/com/ha/sushantrao/opentutor/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.ha.sushantrao.opentutor;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        ImageView h = (ImageView) findViewById(R.id.splashImage);
+        h.setImageResource(R.drawable.icon1);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,LoginTempActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+}
