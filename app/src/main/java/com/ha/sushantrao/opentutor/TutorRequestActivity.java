6400f9cd35e60268e6a3d037e1b7c6c9ad564780
sushant10 <sushdude34@gmail.com>
2018-03-04 00:32:38 -0500

    Added request page

---
+++ app/src/main/java/com/ha/sushantrao/opentutor/TutorRequestActivity.java
@@ -0,0 +1,13 @@
+package com.ha.sushantrao.opentutor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TutorRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_request);
+    }
+}
