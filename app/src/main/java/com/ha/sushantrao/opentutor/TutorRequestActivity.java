ed7b6e98992ee629899e327239d27af05e6557ec
sushant10 <sushdude34@gmail.com>
2018-03-04 01:03:30 -0500

    Sample data

--- app/src/main/java/com/ha/sushantrao/opentutor/TutorRequestActivity.java
+++ app/src/main/java/com/ha/sushantrao/opentutor/TutorRequestActivity.java
@@ -6,10 +6,17 @@ import android.view.MenuItem;
 
 public class TutorRequestActivity extends AppCompatActivity {
 
+    int[] tutorImages;
+    String[] names;
+    String[] requested_class;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_request);
+        tutorImages = new int[]{R.drawable.request_avatar1, R.drawable.request_avatar2, R.drawable.request_avatar3};
+        names = new String[]{"Samuel Riessmann", "Darcy Malone", "Jessica Savarese"};
+        requested_class = new String[]{"Math 132", "Computer Science 121", "Physics 152"};
 
         //back button
         getSupportActionBar().setDisplayShowHomeEnabled(true);
