db1d2b06b5fa4f3b89474c9b2226d377164a1831
Sharath Ramkumar <sharathramkumar@gmail.com>
2018-03-04 13:04:20 -0500

    Update LoginTempActivity.java
    hope this works

--- app/src/main/java/com/ha/sushantrao/opentutor/LoginTempActivity.java
+++ app/src/main/java/com/ha/sushantrao/opentutor/LoginTempActivity.java
@@ -3,23 +3,21 @@ package com.ha.sushantrao.opentutor;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.KeyEvent;
@@ -32,6 +30,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -326,23 +334,29 @@ public class LoginTempActivity extends AppCompatActivity implements LoaderCallba
         protected Boolean doInBackground(Void... params) {
             // TODO: attempt authentication against a network service.
 
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpPost post = new HttpPost("http://localhost:8080/api/login");
+
+            // Create some NameValuePair for HttpPost parameters
+            List<NameValuePair> arguments = new ArrayList<>(2);
+            arguments.add(new BasicNameValuePair("username", mEmail));
+            arguments.add(new BasicNameValuePair("password", mPassword));
+
             try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
+                post.setEntity(new UrlEncodedFormEntity(arguments));
+                HttpResponse response = client.execute(post);
+
+                // Print out the response message
+                System.out.println(EntityUtils.toString(response.getEntity()));
+                System.out.println(response.toString());
+                if(response.toString().contains("200 OK")) {
+                    return true;
+                }
+
+            } catch (IOException e) {
                 return false;
             }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
+            return false;
         }
 
         @Override
